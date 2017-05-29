831286b889a4b05ae2bb3c1f388c494797a0d88f
Cynthia Zanoni <cynthia.zanoni@iwsbrazil.com>
2017-05-29 12:24:14 -0300

    atualização site

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,12 +1,10 @@
-$(document).ready(function() {
-    
-        $('a[href*="#"]:not([href="#"])').click(function() {
-            if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
-                var e = $(this.hash);
-                if (e = e.length ? e : $("[name=" + this.hash.slice(1) + "]"), e.length) return $("html, body").animate({
-                    scrollTop: e.offset().top
-                }, 1e3), !1
-            }
-        })
-    
+$(document).ready(function () {
+    $(".navigationct").click(function () {
+        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+            var a = $(this.hash);
+            if (a = a.length ? a : $("[name=" + this.hash.slice(1) + "]"), $("body").click(), a.length) return $("html,body").animate({
+                scrollTop: a.offset().top
+            }, 1e3), !1
+        }
+    })
 });
